d9614a1134bba3cb34f777105965c1a87f7d2487
grigoryan18 <mariamgrigoryan518@gmail.com>
2023-04-24 17:14:34 +0400

    React shopping cart using UseReducer

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Shop from "./components/Shop";
+import Cart from "./components/Cart";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Cart />
+      <Shop />
+    </div>
+  );
+}
+
+
